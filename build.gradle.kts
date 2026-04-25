8bd8fb8c22ef1aa122462a2258dd9f08e66e7c82
Cibocaz <cibocaz@xyit.net>
2026-04-26 00:07:22 +0800

    初始化项目

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("java")
+}
+
+group = "com.skilfully.etheros"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "papermc-repo"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        name = "sonatype"
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+}
+
+dependencies {
+    compileOnly("com.destroystokyo.paper:paper-api:1.12.2-R0.1-SNAPSHOT")
+}
+
+val targetJavaVersion = 8
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+
+    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible()) {
+        options.release.set(targetJavaVersion)
+    }
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
---
+++ src/main/java/com/skilfully/ethero/EtherosCore.java
@@ -0,0 +1,17 @@
+package com.skilfully.ethero;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EtherosCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
