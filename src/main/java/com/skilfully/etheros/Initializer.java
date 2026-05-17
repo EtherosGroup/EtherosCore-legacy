a0aab0abe3f3c832a2dbbd2859307a0ce1539d3f
IsCibocaz <IsCibocaz@outlook.com>
2026-05-17 20:14:54 +0800

    refactor: 使用DI管理

--- src/main/java/com/skilfully/etheros/Initializer.java
+++ src/main/java/com/skilfully/etheros/Initializer.java
@@ -1,14 +1,30 @@
 package com.skilfully.etheros;
 
-import com.skilfully.etheros.exceptions.NoLanguageException;
 import com.skilfully.etheros.config.ConfigManager;
+import com.skilfully.etheros.data.GlobalData;
+import com.skilfully.etheros.exceptions.NoLanguageException;
+import com.skilfully.etheros.utils.di.annotations.Autowired;
+import com.skilfully.etheros.utils.di.annotations.PostConstruct;
+import com.skilfully.etheros.utils.di.annotations.Service;
+import com.skilfully.etheros.utils.messenger.Messenger;
 
 import java.io.IOException;
 
+@Service
 public class Initializer {
 
-    public static void init() throws IOException, NoLanguageException {
-        ConfigManager.loadConfig();
+    private final Messenger messenger;
+    private final ConfigManager configManager;
+
+    @Autowired
+    public Initializer(Messenger messenger, ConfigManager configManager) {
+        this.messenger = messenger;
+        this.configManager = configManager;
     }
 
+    @PostConstruct
+    public void init() throws IOException, NoLanguageException {
+        messenger.setPluginName(GlobalData.pluginName);
+        configManager.loadConfig();
+    }
 }
